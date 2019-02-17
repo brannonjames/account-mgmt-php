9c2bb9e08bf5ff361cfe7aff990071bed1475143
Jimmy Brannon <user.email>
2019-02-17 12:33:08 -0600

    add request to controller

--- server/app/Http/Controllers/AccountController.php
+++ server/app/Http/Controllers/AccountController.php
@@ -1,13 +1,14 @@
 <?php
 
 namespace App\Http\Controllers;
+use Illuminate\Http\Request;
 
 use App\User;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\DB;
 
 class AccountController extends Controller {
-  public function get() {
+  public function get(Request $request) {
     return DB::select('select * from accounts');
   }
 }
