5374b83701734a268df78ea430014ac6e5a57c42
Jimmy Brannon <user.email>
2019-02-17 16:20:28 -0600

    check new place and add transaction

--- server/app/Http/Controllers/AccountController.php
+++ server/app/Http/Controllers/AccountController.php
@@ -34,7 +34,43 @@ class AccountController extends Controller {
 
   public function update($id, Request $request) {
     try {
-      $account = $request->all();
+      $accountBefore = DB::table('accounts').where('id', $id).get();
+      $accountAfter = $request->all();
+      // if trying to update place, run transaction
+      if ($accountBefore->place != $accountAfter->place) {
+        $active;
+        $transition;
+        if ($accountBefore->place == 'Confirmation' && $accountAfter->place == 'Setup') {
+          $transition_name = 'to_setup';
+          $active = false;
+          $transition = [
+            "name" => 'to_setup',
+            "from" => 'Confirmation',
+            "to" => 'Setup',
+            
+          ];
+        }
+        if ($accountBefore->place == 'Setup' && $accountAfter->place == 'Activated') {
+          $transition_name = 'activate';
+          $active = true;
+        }
+        if ($accountBefore->place == 'Activated' && $accountAfter->place == 'Deactivated') {
+          $transition_name = 'deactivate';
+          $active = false;
+        }
+        if ($accountBefore->place == 'Setup' && $accountAfter->place == 'Deactivated') {
+          $transition_name = 'cancel';
+          $active = false;
+        }
+        DB::transaction(function() {
+          $account->active = $active;
+          DB::table('accounts')->update($account);
+          DB::table('transations')->insert([
+            "name" => $transition_name,
+
+          ]);
+        });
+      }
       DB::table('accounts')->update($account);
       return $account;
     } catch (Exception $e) {
